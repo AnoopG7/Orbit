74bbf9d27c751fa2b8808f6c38e49da670978417
Anoop Gupta <gupta.anoop2006@gmail.com>
2025-09-27 19:50:45 +0530

    Scaffolding

---
+++ firebase/firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB-iT2ti3hk9sHAjdF19DkYFvHiLEnBNcw",
+  authDomain: "orbit-65e7a.firebaseapp.com",
+  projectId: "orbit-65e7a",
+  storageBucket: "orbit-65e7a.firebasestorage.app",
+  messagingSenderId: "669266611289",
+  appId: "1:669266611289:web:6766e429d4013afb70b548",
+};
